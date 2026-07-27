f1036203c1aa9ecc87c19a0fcf35010372bd505a
Alethea-huhuhaha <pintubelakang275@gmail.com>
2026-07-27 17:41:07 +0700

    update login page

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('login');
+    return view('welcome');
 });
+
+Route::get('/login', fn() => view('auth.login'))->name('login');
